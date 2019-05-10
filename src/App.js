8891a6d1dae43712e3028aacd4f8dd5ee359f66d
Xalemg <xalemg@gmail.com>
2019-05-10 21:44:43 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuAppBar from './components/MenuAppBar/MenuAppBar';
+function App() {
+  return (
+    <div className="App">
+        <MenuAppBar/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+    </div>
+  );
+}
+
+export default App;
