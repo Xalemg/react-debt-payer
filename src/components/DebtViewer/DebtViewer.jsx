ebee662ec1419ef98224988a646f045fb1039fc1
Alex <xalemg@gmail.com>
2019-07-19 16:00:06 +0200

    refactor addDebt to be reusable for edits and view

---
+++ src/components/DebtViewer/DebtViewer.jsx
@@ -0,0 +1,165 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import useStyles from './style'
+import CalendarToday from "@material-ui/icons/CalendarTodayOutlined";
+import Typography from '@material-ui/core/Typography';
+import {FormControlLabel, Grid, TextField, Button, Radio, RadioGroup, InputAdornment, IconButton } from '@material-ui/core'
+import { KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
+import {connect} from 'react-redux'
+import { addDebt } from '../../redux/actions/debts/addDebt';
+
+
+
+
+function DebtViewer(props) {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    person: props.debt.person,
+    reason:props.debt.reason,
+    amount: props.debt.amount,
+    description:props.debt.description,
+    payer:props.debt.description,
+  });
+
+
+  const sendDebtToServer = (values,token) => {    
+    props.addDebt(values.person, values.reason,values.amount,values.description, values.date, token)
+  }
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+  const [date, handleDateChange] = React.useState(props.date);
+
+  return (
+   
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            {props.settings.tittle}
+          </Typography>
+          <React.Fragment>
+
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="personName"
+            name="personName"
+            value={values.person}
+            onChange={handleChange('person')}
+            label="Person"
+            fullWidth
+            autoComplete="fname"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="reason"
+            name="reason"
+            value={values.reason}
+            onChange={handleChange('reason')}
+            label="Reason"
+            fullWidth
+            autoComplete="reason"
+          />
+        </Grid>
+        
+        <Grid item xs={12} sm={6}>
+          <TextField
+            className={classes.alignLeft}
+            required
+            id="amount"
+            name="amount"
+            label="Amount"
+            value={values.amount}
+            onChange={handleChange('amount')}
+            fullWidth
+            InputProps={{
+              endAdornment: <InputAdornment position="end">€</InputAdornment>,
+            }}
+            autoComplete="amount"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={12}>
+          <TextField 
+          id="description" 
+          name="description" 
+          label="Description" 
+          value={values.description}
+          onChange={handleChange('description')}
+          fullWidth
+          multiline
+          variant="outlined"
+          rows ="3"
+          />
+        </Grid>
+        <Grid container item xs={12} sm={12}>
+        <Grid item xs={12} sm={6}>
+         <Typography >Who paid?</Typography>
+        <RadioGroup
+          aria-label="Gender"
+          name="gender1"
+          className={classes.group}
+          value={values.payer}
+          onChange={handleChange('payer')}
+        >
+          <FormControlLabel value="negative" control={<Radio   color="primary"/>} label="I got paid" />
+          <FormControlLabel value="positive" control={<Radio />}  label="I paid" />
+        </RadioGroup>
+        </Grid>
+        <Grid item xs={12} sm={6} className={classes.textField}>
+      <Typography gutterBottom>
+        At what time?
+      </Typography >
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDateTimePicker
+        id="date" 
+        name="date"
+        aria-label="date"
+        required
+        inputVariant="outlined"
+        value={date}
+        format="HH:mm    dd/MM/yyyy"
+        showTodayButton
+        onChange={handleDateChange}
+        margin="normal"
+        ampm={false}
+        onError={console.log}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton>
+                <CalendarToday />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        />
+      </MuiPickersUtilsProvider>
+        </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Button color="secondary" variant="outlined" className={classes.button} onClick={() => props.history.goBack()}>
+        Cancel
+        </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Button color="primary" variant="contained" className={classes.button} onClick ={() => sendDebtToServer({...values, date}, props.user.token)}>
+        {props.settings.commitButton}
+      </Button>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+        </Paper>
+  );
+}
+const mapStateToProps = state => {
+  return {
+   user: state.user
+  }
+}
+
+export default connect(mapStateToProps,{addDebt}) (DebtViewer)
--- src/pages/AddDebtPage/AddDebtPage.jsx
+++ src/pages/AddDebtPage/AddDebtPage.jsx
@@ -1,171 +1,50 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import useStyles from './style'
-import CalendarToday from "@material-ui/icons/CalendarTodayOutlined";
-import Typography from '@material-ui/core/Typography';
-import {FormControlLabel, Grid, TextField, Button, Radio, RadioGroup, InputAdornment, IconButton } from '@material-ui/core'
-import { KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import 'date-fns';
 import {connect} from 'react-redux'
 import { addDebt } from '../../redux/actions/debts/addDebt';
+import  DebtViewer  from '../../components/DebtViewer/DebtViewer';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useStyles from './style'
 
 
 
-function AddDebtPage(props) {
+function AddDebtPage() {
   const classes = useStyles();
-  const [values, setValues] = React.useState({
-    person: '',
-    reason:'',
-    amount: Number(30),
-    description:'',
-    payer:""
-  });
 
-
-  const sendDebtToServer = (values,token) => {
-    console.log(token);
-    
-    props.addDebt(values.person, values.reason,values.amount,values.description, values.date, token)
+  const sendDebtToServer = (values,token) => {    
+    this.props.addDebt(values.person, values.reason,values.amount,values.description, values.date, token)
   }
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-  };
-  const [date, handleDateChange] = React.useState(new Date("2019-01-01T18:54"));
 
   return (
+
     <React.Fragment>
       <CssBaseline />
       <Container className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
-            Add new payment
-          </Typography>
-          <React.Fragment>
 
-      <Grid container spacing={5}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="personName"
-            name="personName"
-            value={values.person}
-            onChange={handleChange('person')}
-            label="Person"
-            fullWidth
-            autoComplete="fname"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="reason"
-            name="reason"
-            value={values.reason}
-            onChange={handleChange('reason')}
-            label="Reason"
-            fullWidth
-            autoComplete="reason"
-          />
-        </Grid>
-        
-        <Grid item xs={12} sm={6}>
-          <TextField
-            className={classes.alignLeft}
-            required
-            id="amount"
-            name="amount"
-            label="Amount"
-            value={values.amount}
-            onChange={handleChange('amount')}
-            fullWidth
-            InputProps={{
-              endAdornment: <InputAdornment position="end">€</InputAdornment>,
-            }}
-            autoComplete="amount"
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12}>
-          <TextField 
-          id="description" 
-          name="description" 
-          label="Description" 
-          value={values.description}
-          onChange={handleChange('description')}
-          fullWidth
-          multiline
-          variant="outlined"
-          rows ="3"
-          />
-        </Grid>
-        <Grid container item xs={12} sm={12}>
-        <Grid item xs={12} sm={6}>
-         <Typography >Who paid?</Typography>
-        <RadioGroup
-          aria-label="Gender"
-          name="gender1"
-          className={classes.group}
-          value={values.payer}
-          onChange={handleChange('payer')}
-        >
-          <FormControlLabel value="negative" control={<Radio   color="primary"/>} label="I got paid" />
-          <FormControlLabel value="positive" control={<Radio />}  label="I paid" />
-        </RadioGroup>
-        </Grid>
-        <Grid item xs={12} sm={6} className={classes.textField}>
-      <Typography gutterBottom>
-        At what time?
-      </Typography >
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <KeyboardDateTimePicker
-        id="date" 
-        name="date"
-        aria-label="date"
-        required
-        inputVariant="outlined"
-        value={date}
-        format="HH:mm    dd/MM/yyyy"
-        showTodayButton
-        onChange={handleDateChange}
-        margin="normal"
-        ampm={false}
-        onError={console.log}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton>
-                <CalendarToday />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-        />
-      </MuiPickersUtilsProvider>
-        </Grid>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <Button color="secondary" variant="outlined" className={classes.button} onClick={() => props.history.goBack()}>
-        Cancel
-        </Button>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <Button color="primary" variant="contained" className={classes.button} onClick ={() => sendDebtToServer({...values, date}, props.user.token)}>
-        ADD
-      </Button>
-        </Grid>
-      </Grid>
-    </React.Fragment>
-        </Paper>
+      <DebtViewer
+      debt = {{
+        "person": "",
+        "reason": "",
+        "amount": Number(30),
+        "description": "",
+        "payer": "",
+      }}
+      settings = {{
+        tittle: "Add new payment",
+        commitButton: "ADD",
+        commitAction: sendDebtToServer,
+      }}
+      date = {new Date()}
+      />
       </Container>
     </React.Fragment>
   );
 }
+
 const mapStateToProps = state => {
   return {
    user: state.user
   }
 }
+
 export default connect(mapStateToProps, {addDebt})(AddDebtPage)
