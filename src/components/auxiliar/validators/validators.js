0aa2764bbb4d96f667e3c26746cde5e412066067
Alex <xalemg@gmail.com>
2019-09-07 16:19:03 +0200

    progressing with debtValidator

--- src/components/auxiliar/validators/validators.js
+++ src/components/auxiliar/validators/validators.js
@@ -1,7 +1,8 @@
 
 export const validateDebt = (fields, refs) => {
 
-
+    //eslint-disable-next-line
+    const emailPatter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (fields != null && Array.isArray(fields) && fields.length > 0 ) {
         let wrongfields = [];
@@ -12,10 +13,10 @@ export const validateDebt = (fields, refs) => {
                     error: "Necessary value",
                 });
             }
-            if (field.required) {
+            if (field.name ==="email" && emailPatter.test(field.name) ) {
                 wrongfields.push({
                     field: field.name,
-                    error: "Wrong format",
+                    error: "Wrong email format",
                 });
             }
         });
