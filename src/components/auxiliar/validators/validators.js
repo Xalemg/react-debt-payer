2df87dc2399e7d20ba2b46bb57e74771a4321e61
Alex <xalemg@gmail.com>
2019-09-16 12:27:03 +0200

    still more progress

--- src/components/auxiliar/validators/validators.js
+++ src/components/auxiliar/validators/validators.js
@@ -1,32 +1,29 @@
+//eslint-disable-next-line
 const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const validateDebt = (fields, debtorIsFriend) => {
-
-    //eslint-disable-next-line
-
+export const validateDebt = (fields) => {
 
     if (fields != null && Array.isArray(fields) && fields.length > 0 ) {
         let wrongFields = [];
         fields.forEach(field => {
-
             if (field.required && (field.value == null || field.value === "")) {
                 wrongFields.push({
                     field: field.name,
                     error: "Necessary value",
                 });
             }
-        // Mover al validador de registro
-        //   if (field.name ==="email" && emailPatter.test(field.name) ) {
-        //       wrongfields.push({
-        //           field: field.name,
-        //           error: "Wrong email format",
-        //       });
-        //   }
-        });
-
         return (
             wrongFields
         );
     }
     return null;
+
 }
+// Mover al validador de registro
+    //   if (field.name ==="email" && emailPatter.test(field.name) ) {
+    //       wrongfields.push({
+    //           field: field.name,
+    //           error: "Wrong email format",
+    //       });
+    //   }
+});
